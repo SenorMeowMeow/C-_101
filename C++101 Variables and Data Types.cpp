07e2d8b7ee7578966a5875206bb134cec491c839
SenorMeowMeow <meowzers243@gmail.com>
2016-08-19 20:37:30 -0700

    Add files via upload
    This File Shows different data types being assigned to variable.

---
+++ C++101 Variables and Data Types.cpp
@@ -0,0 +1,22 @@
+// C++101 Variables and Data Types.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+//Data Types
+int Int = 3;
+//^ Variable Type
+bool Bool = true;
+//   ^ Variable Name
+float Float = 1.5;
+//          ^ Variable Assignment
+char Char = 'a';
+//		     ^ Variable Value
+/*
+Other Variable Types
+ - Long (Longer Than an Int)
+ - Long Long (Longer Than a Long)
+ - Double (Longer than a float)
+ - Long Double (Longer Than a Double)
+*/
+
+
